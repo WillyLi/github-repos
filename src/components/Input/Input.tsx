0cbd1bf7e7de45920cd83ba2ec0b1ab97acc648f
willy.li <willy.li@linecorp.com>
2022-05-03 14:35:28 +0800

    feature: create input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,15 @@
+import './Input.css';
+
+interface IInputProps {
+  onChange(val: string): void
+}
+
+function Input({ onChange }: IInputProps) {
+  return (
+    <div className="input">
+      <input type="text" defaultValue="" onChange={(e) => { onChange(e.target.value) }} />
+    </div>
+  );
+}
+
+export default Input;
