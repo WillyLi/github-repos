8242b5851d8893b419facff09506f888a3f2d41f
willy.li <willy.li@linecorp.com>
2022-05-03 14:35:52 +0800

    feature: create list component

---
+++ src/components/List/List.tsx
@@ -0,0 +1,17 @@
+import './List.css';
+import { IRepo } from '../../hooks/useGetSearchRepoAPI/types'
+import Item from '../Item'
+interface IListProps {
+  repoList: IRepo[]
+}
+
+function List({ repoList }: IListProps) {
+  const items = repoList.map(repo => (
+    <Item repo={repo} key={repo.id} />
+  ))
+  return (
+    <ul className="list">{items}</ul>
+  );
+}
+
+export default List;
